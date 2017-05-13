bb51dba4e5e874c181d66da9c3ad4e55a515b368
Martin Krieger <martin@powe.la>
2017-05-12 21:16:10 -0300

    fixed login page

--- js/java.js
+++ js/java.js
@@ -1,5 +1,14 @@
 $(function() {
 
+	$('#register-form-link').click(function(e) {
+
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
     $('#login-form-link').click(function(e) {
 		$("#login-form").delay(100).fadeIn(100);
  		$("#register-form").fadeOut(100);
@@ -7,13 +16,6 @@ $(function() {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-	
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
+
 
 });
